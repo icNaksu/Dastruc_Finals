647c1ccb682b2aefba9417fc7bdfc7524f2cfb22
icNaksu <juliantrocio26@gmail.com>
2025-06-04 01:29:32 +0800

    repository

---
+++ record.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+
+root = tk.Tk()
+
+root.geometry("1920x1080")
+root.title("Class Attendance Record")
+
+label = tk.Label(root, text="hhhhh", font = ('Arial', 18)) 
+
+textbox = tk.Text(root, height=3)
+textbox.pack(padx=10)
+
+
+root.mainloop()
