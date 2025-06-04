64900071d4b13674a82054f686d66fe21ea81c1f
icNaksu <juliantrocio26@gmail.com>
2025-06-04 21:50:20 +0800

    asf

--- record.py
+++ record.py
@@ -5,7 +5,7 @@ root = tk.Tk()
 root.geometry("1920x1080")
 root.title("Class Attendance Record")
 
-label = tk.Label(root, text="hhhhh", font = ('Arial', 18)) 
+label = tk.Label(root, text="Class Attendance Record", font = ('Arial', 18)) 
 
 textbox = tk.Text(root, height=3)
 textbox.pack(padx=10)
